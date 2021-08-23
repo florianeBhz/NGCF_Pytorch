87940620917f0ea1829d0b053116fc53fee290bc
florianeBhz <behanzinf@gmail.com>
2021-08-24 01:42:34 +0200

    Revert "third commit"
    This reverts commit ec6c7ed16a1626a8fb1d01a31892abb7fac3e045.

--- NGCF/main.py
+++ NGCF/main.py
@@ -1,43 +1,64 @@
 import torch
 import torch.optim as optim
 
-from NGCF import MyNGCF, NGCFConv
+from NGCF import NGCF
 from utility.helper import *
 from utility.batch_test import *
 
-import torch
-import torch.optim as optim
-
-import warnings
-warnings.filterwarnings('ignore')
+#import warnings
+#warnings.filterwarnings('ignore')
 from time import time
 
 
 if __name__ == '__main__':
     
-    
-    A, L_I, L = data_generator.get_adj_mat(args['adj_type']) 
-    
-    N,M = data_generator.n_users, data_generator.n_items
-    model = MyNGCF(N,M,L,L_I,device,embed_size,reg,layer_size,batch_size,node_dropout[0],mess_dropout[0]).to(device)
-    
-    #Training--------------------------------------------------------------------------------
-    t0 = time()
-    
-    cur_best_pre_0, stopping_step = 0, 0
-    optimizer = optim.Adam(model.parameters(), lr=args['lr'])
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    verbose = 1
+    batch_size = 1024
+    embed_size = 64
+    Ks = [20, 40, 60, 80, 100]
+    node_dropout = [0.0]
+    mess_dropout = [0.1,0.1,0.1]
 
-    loss_loger, rec_loger, ndcg_loger = [], [], []
+    dataset = "amazon-book" #"gowalla"
+    layer_size = [64,64,64]
+    regs = [1e-5]
+    args = {'device':device,'verbose':verbose,'adj_type':'single', #symetric
+           'batch_size':batch_size, 'embed_size':embed_size,
+           'node_dropout':node_dropout, 'mess_dropout':mess_dropout,
+           'layer_size':layer_size, 'regs':regs, 'data_path':'../Data/',
+            'dataset':dataset,'epoch':500,'Ks':[20, 40, 60, 80, 100],
+            'save_flag':1,'weights_path':'../orig_weights/','results_path':'../my_results/'}
+
+
+    plain_adj, norm_adj, mean_adj = data_generator.get_adj_mat(args.adj_type) #A, L_I, L
+
+
+    model = NGCF(data_generator.n_users,
+                 data_generator.n_items,
+                 norm_adj,
+                 args).to(device)
+
+    t0 = time()
+    """
+    *********************************************************
+    Train.
+    """
+    cur_best_pre_0, stopping_step = 0, 0
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+    loss_loger, pre_loger, rec_loger, ndcg_loger, hit_loger = [], [], [], [], []
     for epoch in range(args['epoch']):
         t1 = time()
         loss, mf_loss, emb_loss = 0., 0., 0.
-        n_batch = data_generator.n_train // args['batch_size'] + 1
+        n_batch = data_generator.n_train // batch_size + 1
 
         for idx in range(n_batch):
             users, pos_items, neg_items = data_generator.sample()
             u_g_embeddings, pos_i_g_embeddings, neg_i_g_embeddings = model(users,
                                                                            pos_items,
-                                                                           neg_items,drop_flag=1)
+                                                                           neg_items,
+                                                                           drop_flag=1)
 
             batch_loss, batch_mf_loss, batch_emb_loss = model.create_bpr_loss(u_g_embeddings,
                                                                               pos_i_g_embeddings,
@@ -51,7 +72,7 @@ if __name__ == '__main__':
             emb_loss += batch_emb_loss
 
         if (epoch + 1) % 10 != 0:
-            if args['verbose'] > 0 and epoch % args['verbose'] == 0:
+            if args['verbose'] > 0 and epoch % verbose == 0:
                 perf_str = 'Epoch %d [%.1fs]: train==[%.5f=%.5f + %.5f]' % (
                     epoch, time() - t1, loss, mf_loss, emb_loss)
                 print(perf_str)
@@ -68,8 +89,7 @@ if __name__ == '__main__':
         ndcg_loger.append(ret['ndcg'])
 
         if args['verbose'] > 0:
-            perf_str = 'Epoch %d [%.1fs + %.1fs]: train==[%.5f=%.5f + %.5f], recall=[%.5f, %.5f], ' \
-                        'ndcg=[%.5f, %.5f]' % \
+            perf_str = 'Epoch %d [%.1fs + %.1fs]: train==[%.5f=%.5f + %.5f], recall=[%.5f, %.5f], , ndcg=[%.5f, %.5f]' % \
                        (epoch, t2 - t1, t3 - t2, loss, mf_loss, emb_loss, ret['recall'][0], ret['recall'][-1],
                         ret['ndcg'][0], ret['ndcg'][-1])
             print(perf_str)
@@ -89,7 +109,9 @@ if __name__ == '__main__':
             print('save the weights in path: ', args['weights_path'] + str(epoch) + '.pkl')
 
     recs = np.array(rec_loger)
+    pres = np.array(pre_loger)
     ndcgs = np.array(ndcg_loger)
+    hit = np.array(hit_loger)
 
     best_rec_0 = max(recs[:, 0])
     idx = list(recs[:, 0]).index(best_rec_0)
@@ -98,17 +120,15 @@ if __name__ == '__main__':
                  (idx, time() - t0, '\t'.join(['%.5f' % r for r in recs[idx]]),
                   '\t'.join(['%.5f' % r for r in ndcgs[idx]]))
     print(final_perf)
-   
-     
-    best_rec_0 = max(recs[:, 0])
-    idx = list(recs[:, 0]).index(best_rec_0)
-
-    save_path = '%soutput/%s/%s.result' % (args['results_path'] % args['dataset'])
+    
+    
+    
+    save_path = '%soutput/%s/%s.result' % (args.results_path, args.dataset)
     f = open(save_path, 'a')
 
     f.write(
         'embed_size=%d, lr=%.4f, layer_size=%s, node_dropout=%s, mess_dropout=%s, regs=%s, adj_type=%s\n\t%s\n'
-        % (args['embed_size'], 0.0001, args['layer_size'], args['node_dropout'], args['mess_dropout'], args['regs'],
-           args['adj_type'], final_perf))
+        % (args.embed_size, 0.0001, args.layer_size, args.node_dropout, args.mess_dropout, args.regs,
+           args.adj_type, final_perf))
     
     f.close()
